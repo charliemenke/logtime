a609e29fbba622d7ba48dc7727641e5305347f47
Charlie Menke <charliemenke@Charlies-MacBook-Pro.local>
2021-12-23 17:46:46 -0700

    initial commit

---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"time"
+)
+
+func main() {
+
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Printf("Error reading $HOME: %s", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("here 0")
+	// grab command line args
+	setConfigPath := flag.String("word", "", "Path for where timelog file is stored")
+	flag.Parse()
+	fmt.Println("here 0.5")
+
+	// open cofig file
+	f, err := os.OpenFile(homeDir+"/.logtime", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Printf("Error opening ~/.logtime file: %s", err)
+		os.Exit(1)
+	}
+	defer f.Close()
+	fmt.Println("here 0.75")
+
+	timeLogPath, err := ioutil.ReadFile(homeDir + "/.logtime")
+	if err != nil {
+		fmt.Printf("Error reading ~/.logtime file: %s", err)
+		os.Exit(1)
+	}
+	// write default path if not set
+	if len(timeLogPath) == 0 {
+		fmt.Println("here 1")
+		timeLogPath = []byte(homeDir + "/timelog.txt")
+		_, err = f.WriteString(homeDir + "/timelog.txt")
+		if err != nil {
+			fmt.Printf("Error writing to config file: %s", err)
+		}
+		fmt.Println("here 2")
+	}
+	// check if we are setting timelog file locationgo env
+	var toLog []string
+	if *setConfigPath != "" {
+		toLog = os.Args[2:]
+		// write new path to ~/.logtime
+		_, err = f.WriteString(*setConfigPath)
+		if err != nil {
+			fmt.Printf("Error writing to ~/.logtime file: %s", err)
+			os.Exit(1)
+		}
+		timeLogPath = []byte(*setConfigPath)
+	} else {
+		toLog = os.Args[1:]
+	}
+
+	// write to timelog
+	timeLogFile, err := os.OpenFile(string(timeLogPath), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Printf("Error opening timelog file: %s", err)
+		os.Exit(1)
+	}
+	defer timeLogFile.Close()
+	logString := ""
+	for _, word := range toLog {
+		logString = logString + word + " "
+	}
+	timeLogFile.WriteString(time.Now().Format(time.ANSIC) + ": " + logString + "\n")
+	os.Exit(0)
+}
